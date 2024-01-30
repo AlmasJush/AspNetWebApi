7e35c813134faceed74dcdf359f95fe1b64c3705
Jasur <Jasur@DESKTOP-AABT8NI>
2024-01-30 16:09:49 -0500

    Add project files.

---
+++ ContactsApi/Controllers/ContactsController.cs
@@ -0,0 +1,77 @@
+﻿using ContactsApi.Data;
+using ContactsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApi.Controllers
+{
+    [ApiController]
+    [Route("api/contacts")]
+    //[Route("api/[controller]")] // this line exact same as line6 [controller] = contacts
+    public class ContactsController : Controller
+    {
+        private readonly ContacsApiDbContext dbContext;
+
+        public ContactsController(ContacsApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContacts()
+        {
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FullName = addContactRequest.FullName,
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                Phone = addContactRequest.Phone,
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+
+            if (contact != null)
+            {
+                contact.FullName = updateContactRequest.FullName;
+                contact.Address = updateContactRequest.Address;
+                contact.Email = updateContactRequest.Email;
+                contact.Phone = updateContactRequest.Phone;
+
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                
+            }
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ ContactsApi/Data/ContacsApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContactsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApi.Data
+{
+    public class ContacsApiDbContext : DbContext
+    {
+        public ContacsApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
